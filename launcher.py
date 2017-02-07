3caa355d6ba66cecaa3a08c99859b1d385944ee8
David Booth <worlds6440@aol.com>
2017-02-07 23:26:33 +0000

    Added Z button neutral and motor disable feature to launcher code.

--- launcher.py
+++ launcher.py
@@ -49,24 +49,24 @@ class launcher:
             #    print("Failed to get Nunchuck")
 
             # Show joystick state
-            try:
-                joystick_state = self.wiimote.get_joystick_state()
-                if joystick_state:
-                    print("joystick_state: {0}".format(joystick_state))
-            except:
-                print("Failed to get Joystick")
+            # try:
+            #     joystick_state = self.wiimote.get_joystick_state()
+            #     if joystick_state:
+            #         print("joystick_state: {0}".format(joystick_state))
+            # except:
+            #     print("Failed to get Joystick")
 
-            try:
-                l_joystick_state = \
-                    self.wiimote.get_classic_joystick_state(True)
-                r_joystick_state = \
-                    self.wiimote.get_classic_joystick_state(False)
-                if l_joystick_state:
-                    print("l_joystick_state: {0}".format(l_joystick_state))
-                if r_joystick_state:
-                    print("r_joystick_state: {0}".format(r_joystick_state))
-            except:
-                print("Failed to get Joystick")
+            # try:
+            #     l_joystick_state = \
+            #         self.wiimote.get_classic_joystick_state(True)
+            #     r_joystick_state = \
+            #         self.wiimote.get_classic_joystick_state(False)
+            #     if l_joystick_state:
+            #         print("l_joystick_state: {0}".format(l_joystick_state))
+            #     if r_joystick_state:
+            #         print("r_joystick_state: {0}".format(r_joystick_state))
+            # except:
+            #     print("Failed to get Joystick")
 
             if buttons_state is not None:
                 if (buttons_state & cwiid.BTN_A):
@@ -85,38 +85,45 @@ class launcher:
                     print("BUTTON_RIGHT")
 
             if classic_buttons_state is not None:
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_UP):
-                    print("KEY_UP")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_DOWN):
-                    print("KEY_DOWN")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_LEFT):
-                    print("KEY_LEFT")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_RIGHT):
-                    print("KEY_RIGHT")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_A):
-                    print("KEY_A")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_B):
-                    print("KEY_B")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_X):
-                    print("KEY_X")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_Y):
-                    print("KEY_Y")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_ZL):
-                    print("KEY_Z_LEFT")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_ZR):
-                    print("KEY_Z_RIGHT")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_L):
-                    print("KEY_L")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_R):
-                    print("KEY_R")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_PLUS):
-                    print("KEY_PLUS")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_MINUS):
-                    print("KEY_MINUS")
-                if (classic_buttons_state & cwiid.CLASSIC_BTN_HOME):
-                    print("KEY_HOME")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_UP):
+                #     print("KEY_UP")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_DOWN):
+                #     print("KEY_DOWN")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_LEFT):
+                #     print("KEY_LEFT")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_RIGHT):
+                #     print("KEY_RIGHT")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_A):
+                #     print("KEY_A")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_B):
+                #     print("KEY_B")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_X):
+                #     print("KEY_X")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_Y):
+                #     print("KEY_Y")
+
+                if (classic_buttons_state & cwiid.CLASSIC_BTN_ZL or
+                        classic_buttons_state & cwiid.CLASSIC_BTN_ZR):
+                    # One of the Z buttons pressed, disable
+                    # motors and set neutral.
+                    self.core.enable_motors(False)
+                else:
+                    # Neither Z buttons pressed, allow motors to move freely.
+                    self.core.enable_motors(True)
+
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_L):
+                #     print("KEY_L")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_R):
+                #     print("KEY_R")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_PLUS):
+                #     print("KEY_PLUS")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_MINUS):
+                #     print("KEY_MINUS")
+                # if (classic_buttons_state & cwiid.CLASSIC_BTN_HOME):
+                #     print("KEY_HOME")
 
             time.sleep(0.05)
+
 
 if __name__ == "__main__":
     launcher = launcher()
