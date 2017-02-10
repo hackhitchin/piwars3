41d67221a40792b86cf80d112a4f52a9a6dc4291
PKM-UK <pyrophernalia@gmail.com>
2017-02-10 12:55:48 +0000

    Refactor and rename tito_logo to WallFollower
    Break out magic numbers into named constants. Rename class to reflect that it isn't doing LOGO things any more

--- wall_follower.py
+++ wall_follower.py
@@ -4,28 +4,34 @@ import time
 # import sounds
 
 
-class TitoLogo:
+class WallFollower:
     def __init__(self, core_module):
         """Class Constructor"""
         self.killed = False
         self.core = core_module
         self.ticks = 0
+        self.tick_time = 0.1 # How many seconds per control loop
+        self.time_limit = 10 # How many seconds to run for
+        self.left_motor_scale = 0.3  # What proportion of full throttle to use
+        self.right_motor_scale = 0.3
 
     def stop(self):
         """Simple method to stop the RC loop"""
         self.killed = True
 
-    def run_auto(self):
-        self.core.enable_motors(True)
+    def run(self):
         """Read a sensor and set motor speeds accordingly"""
-
-        while not self.killed and self.ticks < 100:
+        self.core.enable_motors(True)
+        
+        tick_limit = self.tick_time * self.time_limit
+        
+        while not self.killed and self.ticks < tick_limit:
             prox = self.drive.read_sensor()
 
-            # sensor measures distance to left wall
+            # sensor measures distance to left wall (0 - 20cm)
             # so lower sensor value means more left motor needed
-            self.leftspeed = (40 - prox) / 40.0
-            self.rightspeed = (prox / 40.0) + 0.5
+            self.leftspeed = ((20 - prox) / 20) * self.left_motor_scale
+            self.rightspeed = (prox / 20) * self.right_motor_scale
 
             self.core.throttle(self.leftspeed, self.rightspeed)
             print ("Motors %f, %f" % (self.leftspeed, self.rightspeed))
@@ -36,12 +42,12 @@ class TitoLogo:
 
 if __name__ == "__main__":
     core = core.Core()
-    tito_logo = TitoLogo(core)
+    follower = WallFollower(core)
     try:
-        tito_logo.run_auto()
+        follower.run()
     except (KeyboardInterrupt) as e:
         # except (Exception, KeyboardInterrupt) as e:
         # Stop any active threads before leaving
-        tito_logo.stop()
+        follower.stop()
         core.stop()
         print("Quitting")
