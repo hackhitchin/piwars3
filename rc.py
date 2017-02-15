502be07d3823cbe6927ab0b9ad4c3652fa02d64c
David Booth <worlds6440@aol.com>
2017-02-15 21:39:26 +0000

    RC Modified to show min/max raw joystick values

--- rc.py
+++ rc.py
@@ -9,6 +9,16 @@ class rc:
         self.core_module = core_module
         self.ticks = 0
 
+        # Store Max joystick values for left/right
+        self.l_max_x = -1
+        self.l_min_x = -1
+        self.l_max_y = -1
+        self.l_min_y = -1
+        self.r_max_x = -1
+        self.r_min_x = -1
+        self.r_max_y = -1
+        self.r_min_y = -1
+
     def stop(self):
         """Simple method to stop the RC loop"""
         self.killed = True
@@ -28,11 +38,67 @@ class rc:
             except:
                 print("Failed to get Joystick")
 
+            # Get raw joystick values. Using it to calibrate min/max range
+            l_joystick_raw_pos = l_joystick_state['state']['raw']
+            l_joystick_y, l_joystick_x = l_joystick_raw_pos
+            # min/max [X]
+            if self.l_max_x == -1:
+                self.l_max_x = l_joystick_x
+            else:
+                self.l_max_x = max(self.l_max_x, l_joystick_x)
+            if self.l_min_x == -1:
+                self.l_min_x = l_joystick_x
+            else:
+                self.l_min_x = max(self.l_min_x, l_joystick_x)
+            # min/max [Y]
+            if self.l_max_y == -1:
+                self.l_max_y = l_joystick_x
+            else:
+                self.l_max_y = max(self.l_max_y, l_joystick_x)
+            if self.l_min_y == -1:
+                self.l_min_y = l_joystick_x
+            else:
+                self.l_min_y = max(self.l_min_y, l_joystick_x)
+
+            r_joystick_raw_pos = r_joystick_state['state']['raw']
+            r_joystick_y, r_joystick_x = r_joystick_raw_pos
+            # min/max [X]
+            if self.r_max_x == -1:
+                self.r_max_x = r_joystick_x
+            else:
+                self.r_max_x = max(self.r_max_x, r_joystick_x)
+            if self.r_min_x == -1:
+                self.r_min_x = r_joystick_x
+            else:
+                self.r_min_x = max(self.r_min_x, r_joystick_x)
+            # min/max [Y]
+            if self.r_max_y == -1:
+                self.r_max_y = r_joystick_x
+            else:
+                self.r_max_y = max(self.r_max_y, r_joystick_x)
+            if self.r_min_y == -1:
+                self.r_min_y = r_joystick_x
+            else:
+                self.r_min_y = max(self.r_min_y, r_joystick_x)
+
+            print("Left raw X[{},{}] Y[{},{}]".format(
+                self.r_min_x,
+                self.r_max_x,
+                self.r_min_y,
+                self.r_max_y)
+            )
+            print("Right raw X[{},{}] Y[{},{}]".format(
+                self.r_min_x,
+                self.r_max_x,
+                self.r_min_y,
+                self.r_max_y)
+            )
+
             # Annotate joystick states to screen
-            if l_joystick_state:
-                print("l_joystick_state: {}".format(l_joystick_state))
-            if r_joystick_state:
-                print("r_joystick_state: {}".format(r_joystick_state))
+            # if l_joystick_state:
+            #     print("l_joystick_state: {}".format(l_joystick_state))
+            # if r_joystick_state:
+            #     print("r_joystick_state: {}".format(r_joystick_state))
 
             # Grab normalised x,y / steering,throttle
             # from left and right joysticks.
@@ -42,7 +108,7 @@ class rc:
             r_throttle, r_steering = r_joystick_pos
 
             self.core_module.throttle(self.l_throttle, self.r_throttle)
-            print ("Motors %f, %f" % (self.l_throttle, self.r_throttle))
+            # print ("Motors %f, %f" % (self.l_throttle, self.r_throttle))
 
             # Sleep between loops to allow other stuff to
             # happen and not over burden Pi and Arduino.
