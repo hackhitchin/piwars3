693eb821142b48c3e0fc4533600b9f6da42bf494
PKM-UK <pyrophernalia@gmail.com>
2017-02-16 00:35:59 +0000

    Add PID to wall follower code

--- wall_follower.py
+++ wall_follower.py
@@ -1,10 +1,12 @@
 # Import triangula module to interact with SixAxis
 import core
 import time
+import PID
 # import sounds
 
 ''' 10-2-2017: This code is completely untested; don't be surprised when it 
 doesn't compile, run or do anything sensible.'''
+
 
 class WallFollower:
     def __init__(self, core_module):
@@ -15,9 +17,14 @@ class WallFollower:
         self.tick_time = 0.1 # How many seconds per control loop
         self.time_limit = 10 # How many seconds to run for
 
+        self.pidc = PID.PID(0.2, 0.0, 0.05)
+
     def stop(self):
         """Simple method to stop the RC loop"""
         self.killed = True
+
+    def set_control_mode(self, mode):
+        self.control_mode = mode
 
     def decide_speeds(self, sensorvalue):
         """ Set up return values at the start"""
@@ -25,62 +32,81 @@ class WallFollower:
         rightspeed = 0
 
         if self.control_mode == "LINEAR":
+            speed_mid = 0.3
+            speed_range = 0.3            
             """ Deviation is distance from intended midpoint.
                 Right is positive, left is negative
                 Rate is how much to add/subtract from motor speed """
 
             distance_midpoint = 10
-            deviation = (sensorvalue - 10) / 10.0 # [-1, 1]
+            deviation = (sensorvalue - 10) / 10.0  # [-1, 1]
 
-            speed_mid = 0.3
-            speed_range = 0.3
-
-            leftspeed = (speed_mid - (deviation * speed_range) )
-            rightspeed = (speed_mid + (deviation * speed_range) )
+            leftspeed = (speed_mid - (deviation * speed_range))
+            rightspeed = (speed_mid + (deviation * speed_range))
 
             return leftspeed, rightspeed
 
         elif self.control_mode == "EXPO":
+            speed_mid = 0.3
+            speed_range = 0.3
+
             distance_midpoint = 10
-            deviation = (sensorvalue - 10) / 10.0 # [-1, 1]
+            deviation = (sensorvalue - distance_midpoint) / 10.0  # [-1, 1]
 
             if (deviation < 0):
                 deviation = 0 - (deviation * deviation)
             else:
                 deviation = deviation * deviation
 
+            leftspeed = (speed_mid - (deviation * speed_range))
+            rightspeed = (speed_mid + (deviation * speed_range))
+
+        elif self.control_mode == "PID":
             speed_mid = 0.3
             speed_range = 0.3
 
-            leftspeed = (speed_mid - (deviation * speed_range) )
-            rightspeed = (speed_mid + (deviation * speed_range) )
-            
+            distance_midpoint = 10
+            error = (sensorvalue - distance_midpoint)
+            self.pidc.update(error)
+
+            deviation = self.pidc.output
+
+            print("PID out: %f" % deviation)
+
+            leftspeed = (speed_mid - (deviation * speed_range))
+            rightspeed = (speed_mid + (deviation * speed_range))
+
         else:
             leftspeed = speed_mid
             leftspeed = speed_mid
 
         return leftspeed, rightspeed
 
-
     def run(self):
         """Read a sensor and set motor speeds accordingly"""
         self.core.enable_motors(True)
-        
-        tick_limit = self.tick_time * self.time_limit
-        
+
+        tick_limit = self.time_limit / self.tick_time
+
+        self.set_control_mode("PID")
+
         while not self.killed and self.ticks < tick_limit:
-            prox = self.drive.read_sensor()
+            prox = self.core.read_sensor()
+            leftspeed = 0
+            rightspeed = 0
 
             # sensor measures distance to left wall (0 - 20cm)
             # so lower sensor value means more left motor needed
-            
-            leftspeed, rightspeed = decide_speeds(prox, "EXPO")
 
-            self.core.throttle(self.leftspeed, self.rightspeed)
-            print ("Motors %f, %f" % (self.leftspeed, self.rightspeed))
+            leftspeed, rightspeed = self.decide_speeds(prox)
+
+            self.core.throttle(leftspeed, rightspeed)
+            print("Motors %f, %f" % (leftspeed, rightspeed))
 
             self.ticks = self.ticks + 1
             time.sleep(0.1)
+
+        print("Ticks %d" % self.ticks)
 
 
 if __name__ == "__main__":
